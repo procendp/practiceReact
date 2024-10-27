120f668f591dc294eb6050bc43d747515dc30db5
procendp <procendp@gmail.com>
2024-10-27 23:52:44 +0900

    practicing about cleanup function

--- react-for-me/src/App.js
+++ react-for-me/src/App.js
@@ -1,33 +1,24 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+  function byeFn() {
+    console.log("Bye :(");
+  }
+  function hiFn() {
+    console.log("Hi :)");
+    return byeFn; // component가 파괴될 때도 function을 실행하고 싶다면 byeFn을 return 해야함
+  }
+  useEffect(hiFn, []); // useEffect에서 function 받고, 이 function은 dependency가 변화할 때 호출됨
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  // console.log("i run all the time");
-  useEffect(() => {
-    console.log("I run only once.");
-  }, []); // [] : empty 상태.. 코드 한 번만 실행
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.", keyword);
-  }, [keyword]); // keyword를 지켜봐!!!  - keyword가 변화할 때만 코드 실행.. 이전처럼 counter 바뀔 때 같이 실행되지 않음
-  useEffect(() => {
-    console.log("I run when 'counter' changes.", keyword);
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when keyword & counter changes.", keyword);
-  }, [keyword, counter]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev); // setShowing 통해서 이전 value 받아오고, 그 value의 반대값 return
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here"
-      ></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
